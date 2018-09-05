ead3e585b8a61c9367fdf4482bbf9f1ecab6afde
Nauris Linde <naurislinde@gmail.com>
2018-09-05 19:56:57 +0300

    Updated to Laravel 5.7

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,6 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="env" content="{{ app()->environment() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 
