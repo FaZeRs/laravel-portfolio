be1ea73015e197c628d2b727fe499299cd636719
Nauris Linde <naurislinde@gmail.com>
2019-02-01 16:55:19 +0200

    Removed dynamic imports

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,15 +1,38 @@
-const Index = () => import('~/pages/index').then(m => m.default || m)
-const Experience = () => import('~/pages/experience').then(m => m.default || m)
-const Services = () => import('~/pages/services').then(m => m.default || m)
-const Portfolio = () => import('~/pages/portfolio').then(m => m.default || m)
-const Contact = () => import('~/pages/contact').then(m => m.default || m)
-const NotFound = () => import('~/pages/errors/404').then(m => m.default || m)
+import Index from '~/pages/index'
+import Experience from '~/pages/experience'
+import Services from '~/pages/services'
+import Portfolio from '~/pages/portfolio'
+import Contact from '~/pages/contact'
+import NotFound from '~/pages/errors/404'
 
 export default [
-  { path: '/', name: 'index', component: Index },
-  { path: '/experience', name: 'experience', component: Experience },
-  { path: '/services', name: 'services', component: Services },
-  { path: '/portfolio', name: 'portfolio', component: Portfolio },
-  { path: '/contact', name: 'contact', component: Contact },
-  { path: '*', component: NotFound }
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  },
+  {
+    path: '/experience',
+    name: 'experience',
+    component: Experience
+  },
+  {
+    path: '/services',
+    name: 'services',
+    component: Services
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: Portfolio
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  },
+  {
+    path: '*',
+    component: NotFound
+  }
 ]
