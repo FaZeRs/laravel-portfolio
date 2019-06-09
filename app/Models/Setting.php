30f520a8cb2c331a015bd75141701b9207a4d5c3
Nauris Linde <naurislinde@gmail.com>
2019-06-09 13:50:50 +0300

    Override Setting get() function to fix translations

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -43,6 +43,26 @@ class Setting extends SettingModel
         });
     }
 
+    /**
+     * Grab a setting value from the database.
+     *
+     * @param string $key The setting key, as defined in the key db column
+     *
+     * @return string The setting value.
+     */
+    public static function get($key)
+    {
+
+        $setting = new self();
+        $entry = $setting->where('key', $key)->first();
+
+        if (!$entry) {
+            return;
+        }
+
+        return $entry->value;
+    }
+
     public function setValueAttribute($value)
     {
         $field = json_decode($this->field, true);
