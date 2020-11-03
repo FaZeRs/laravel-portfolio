ce04d0fd4243c627f93c91aede4fe89e04cf0c44
Nauris Linde <naurislinde@gmail.com>
2020-11-03 23:31:52 +0200

    Created Education and Experience tests

---
+++ tests/Feature/EducationTest.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Education;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EducationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function test_guest_cannot_create_an_education()
+    {
+        $data = [
+            'qualification' => $this->faker->sentence(4),
+            'organisation' => $this->faker->sentence(2),
+            'from' => $this->faker->date,
+            'to' => $this->faker->date,
+            'ongoing' => $this->faker->boolean(25),
+        ];
+
+        $response = $this->json('POST', route('api.education.store'), $data);
+        $response->assertUnauthorized();
+    }
+
+    public function test_user_cannot_create_an_education()
+    {
+        $this->loginAsUser();
+
+        $data = [
+            'qualification' => $this->faker->sentence(4),
+            'organisation' => $this->faker->sentence(2),
+            'from' => $this->faker->date,
+            'to' => $this->faker->date,
+            'ongoing' => $this->faker->boolean(25),
+        ];
+
+        $response = $this->json('POST', route('api.education.store'), $data);
+        $response->assertUnauthorized();
+    }
+
+    public function test_admin_can_create_an_education()
+    {
+        $this->loginAsAdmin();
+
+        $data = [
+            'qualification' => $this->faker->sentence(4),
+            'organisation' => $this->faker->sentence(2),
+            'from' => $this->faker->date,
+            'to' => $this->faker->date,
+            'ongoing' => $this->faker->boolean(25),
+        ];
+
+        $response = $this->json('POST', route('api.education.store'), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'id',
+            'qualification',
+            'organisation',
+            'from',
+            'to',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+
+    public function test_guest_cannot_edit_an_education()
+    {
+        $education = Education::factory()->create();
+
+        $data = [
+            'qualification' => $this->faker->sentence(4),
+        ];
+        $response = $this->json('PUT', route('api.education.update', $education), $data);
+        $response->assertUnauthorized();
+    }
+
+    public function test_user_cannot_edit_an_education()
+    {
+        $this->loginAsUser();
+
+        $education = Education::factory()->create();
+
+        $data = [
+            'qualification' => $this->faker->sentence(4),
+        ];
+        $response = $this->json('PUT', route('api.education.update', $education), $data);
+        $response->assertUnauthorized();
+    }
+
+    public function test_admin_can_edit_an_education()
+    {
+        $this->loginAsAdmin();
+
+        $education = Education::factory()->create();
+
+        $data = [
+            'title' => $this->faker->sentence(4),
+        ];
+        $response = $this->json('PUT', route('api.education.update', $education), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'id',
+            'qualification',
+            'organisation',
+            'from',
+            'to',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+
+    public function test_guest_cannot_delete_an_education()
+    {
+        $education = Education::factory()->create();
+
+        $response = $this->json('DELETE', route('api.education.destroy', $education));
+        $response->assertUnauthorized();
+    }
+
+    public function test_user_cannot_delete_an_education()
+    {
+        $this->loginAsUser();
+
+        $education = Education::factory()->create();
+
+        $response = $this->json('DELETE', route('api.education.destroy', $education));
+        $response->assertUnauthorized();
+    }
+
+    public function test_admin_can_delete_an_education()
+    {
+        $this->loginAsAdmin();
+
+        $education = Education::factory()->create();
+        $response = $this->json('DELETE', route('api.education.destroy', $education));
+        $response->assertSuccessful();
+        $this->assertSoftDeleted($education);
+    }
+
+    public function test_admin_can_restore_an_education()
+    {
+        $this->loginAsAdmin();
+        $education = Education::factory()->softDeleted()->create();
+        $response = $this->json('PUT', route('api.education.restore', $education));
+        $response->assertSuccessful();
+    }
+
+    public function test_admin_can_delete_permanently_an_education()
+    {
+        $this->loginAsAdmin();
+        $education = Education::factory()->softDeleted()->create();
+        $response = $this->json('PUT', route('api.education.delete-permanently', $education));
+        $response->assertSuccessful();
+        $this->assertDeleted($education);
+    }
+
+    public function test_get_education()
+    {
+        Education::factory()->count(5)->create();
+        $response = $this->json('GET', route('api.education.index'));
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            '*' => [
+                'id',
+                'qualification',
+                'organisation',
+                'from',
+                'to',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+
+    public function test_get_single_education()
+    {
+        $education = Education::factory()->create();
+        $response = $this->json('GET', route('api.education.show', $education));
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'id',
+            'qualification',
+            'organisation',
+            'from',
+            'to',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+}
---
+++ tests/Feature/ExperienceTest.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Experience;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ExperienceTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function test_guest_cannot_create_an_experience()
+    {
+        $data = [
+            'position' => $this->faker->jobTitle,
+            'employer' => $this->faker->company,
+            'website' => $this->faker->domainName,
+            'from' => $this->faker->date,
+            'to' => $this->faker->date,
+            'ongoing' => $this->faker->boolean(25),
+        ];
+
+        $response = $this->json('POST', route('api.experience.store'), $data);
+        $response->assertUnauthorized();
+    }
+
+    public function test_user_cannot_create_an_experience()
+    {
+        $this->loginAsUser();
+
+        $data = [
+            'position' => $this->faker->jobTitle,
+            'employer' => $this->faker->company,
+            'website' => $this->faker->domainName,
+            'from' => $this->faker->date,
+            'to' => $this->faker->date,
+            'ongoing' => $this->faker->boolean(25),
+        ];
+
+        $response = $this->json('POST', route('api.experience.store'), $data);
+        $response->assertUnauthorized();
+    }
+
+    public function test_admin_can_create_an_experience()
+    {
+        $this->loginAsAdmin();
+
+        $data = [
+            'position' => $this->faker->jobTitle,
+            'employer' => $this->faker->company,
+            'website' => $this->faker->domainName,
+            'from' => $this->faker->date,
+            'to' => $this->faker->date,
+            'ongoing' => $this->faker->boolean(25),
+        ];
+
+        $response = $this->json('POST', route('api.experience.store'), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'id',
+            'position',
+            'employer',
+            'website',
+            'from',
+            'to',
+            'ongoing',
+            'logo',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+
+    public function test_guest_cannot_edit_an_experience()
+    {
+        $experience = Experience::factory()->create();
+
+        $data = [
+            'position' => $this->faker->jobTitle,
+        ];
+        $response = $this->json('PUT', route('api.experience.update', $experience), $data);
+        $response->assertUnauthorized();
+    }
+
+    public function test_user_cannot_edit_an_experience()
+    {
+        $this->loginAsUser();
+
+        $experience = Experience::factory()->create();
+
+        $data = [
+            'position' => $this->faker->jobTitle,
+        ];
+        $response = $this->json('PUT', route('api.experience.update', $experience), $data);
+        $response->assertUnauthorized();
+    }
+
+    public function test_admin_can_edit_an_experience()
+    {
+        $this->loginAsAdmin();
+
+        $experience = Experience::factory()->create();
+
+        $data = [
+            'position' => $this->faker->jobTitle,
+        ];
+        $response = $this->json('PUT', route('api.experience.update', $experience), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'id',
+            'position',
+            'employer',
+            'website',
+            'from',
+            'to',
+            'ongoing',
+            'logo',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+
+    public function test_guest_cannot_delete_an_experience()
+    {
+        $experience = Experience::factory()->create();
+
+        $response = $this->json('DELETE', route('api.experience.destroy', $experience));
+        $response->assertUnauthorized();
+    }
+
+    public function test_user_cannot_delete_an_experience()
+    {
+        $this->loginAsUser();
+
+        $experience = Experience::factory()->create();
+
+        $response = $this->json('DELETE', route('api.experience.destroy', $experience));
+        $response->assertUnauthorized();
+    }
+
+    public function test_admin_can_delete_an_experience()
+    {
+        $this->loginAsAdmin();
+
+        $experience = Experience::factory()->create();
+        $response = $this->json('DELETE', route('api.experience.destroy', $experience));
+        $response->assertSuccessful();
+        $this->assertSoftDeleted($experience);
+    }
+
+    public function test_admin_can_restore_an_experience()
+    {
+        $this->loginAsAdmin();
+        $experience = Experience::factory()->softDeleted()->create();
+        $response = $this->json('PUT', route('api.experience.restore', $experience));
+        $response->assertSuccessful();
+    }
+
+    public function test_admin_can_delete_permanently_an_experience()
+    {
+        $this->loginAsAdmin();
+        $experience = Experience::factory()->softDeleted()->create();
+        $response = $this->json('PUT', route('api.experience.delete-permanently', $experience));
+        $response->assertSuccessful();
+        $this->assertDeleted($experience);
+    }
+
+    public function test_get_education()
+    {
+        Experience::factory()->count(5)->create();
+        $response = $this->json('GET', route('api.experience.index'));
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            '*' => [
+                'id',
+                'position',
+                'employer',
+                'website',
+                'from',
+                'to',
+                'ongoing',
+                'logo',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+
+    public function test_get_single_experience()
+    {
+        $experience = Experience::factory()->create();
+        $response = $this->json('GET', route('api.experience.show', $experience));
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'id',
+            'position',
+            'employer',
+            'website',
+            'from',
+            'to',
+            'ongoing',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+}
