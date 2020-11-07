ee222404dfb8bb339af6b090481039de5b838455
Nauris Linde <naurislinde@gmail.com>
2020-11-07 17:04:12 +0200

    Created login and register tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,12 +2,82 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
+
+    public function testUserRegisterValidation()
+    {
+        $response = $this->json('POST', '/api/register', []);
+        $response->assertStatus(422);
+    }
+
+    public function testUserCanRegister()
+    {
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ];
+        $response = $this->json('POST', '/api/register', $data);
+        $response->assertSuccessful();
+    }
+
+    public function testUserRegisterUniqueEmail()
+    {
+        User::factory()->create(['email' => 'john@doe.com']);
+        $data = [
+            'name' => $this->faker->name,
+            'email' => 'john@doe.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ];
+        $response = $this->json('POST', '/api/register', $data);
+        $response->assertStatus(422);
+    }
+
+    public function testUserLoginValidation()
+    {
+        $response = $this->json('POST', '/api/login', []);
+        $response->assertStatus(422);
+    }
+
+    public function testUserLoginWrongPassword()
+    {
+        User::factory()->create([
+            'email' => 'john@doe.com',
+            'password' => 'secret',
+        ]);
+        $response = $this->json('POST', '/api/login', [
+            'email' => 'john@doe.com',
+            'password' => 'password',
+        ]);
+        $response->assertStatus(422);
+    }
+
+    public function testUserCanLogin()
+    {
+        User::factory()->create([
+            'email' => 'john@doe.com',
+            'password' => 'secret',
+        ]);
+        $response = $this->json('POST', '/api/login', [
+            'email' => 'john@doe.com',
+            'password' => 'secret',
+        ]);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'token',
+            'user',
+        ]);
+    }
 
     public function testUserGetDetails()
     {
