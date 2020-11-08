5fdd3e127628cd05bfa7bb77714a344232eaf85c
Nauris Linde <naurislinde@gmail.com>
2020-11-08 23:46:55 +0200

    Cleanup

--- client/pages/admin/login.vue
+++ client/pages/admin/login.vue
@@ -55,53 +55,6 @@
       </v-flex>
     </v-layout>
   </v-container>
-  <!--  <section>-->
-  <!--    <v-container grid-list-md>-->
-  <!--      <validation-observer-->
-  <!--        ref="observer"-->
-  <!--        v-slot="{ invalid, validated, passes }"-->
-  <!--      >-->
-  <!--        <form @submit.prevent="passes(onSubmit)">-->
-  <!--          <validation-provider-->
-  <!--            v-slot="{ errors }"-->
-  <!--            name="email"-->
-  <!--            rules="required|email"-->
-  <!--          >-->
-  <!--            <v-text-field-->
-  <!--              v-model="form.email"-->
-  <!--              :error-messages="errors"-->
-  <!--              :label="$t('fields.email')"-->
-  <!--              required-->
-  <!--            ></v-text-field>-->
-  <!--          </validation-provider>-->
-  <!--          <validation-provider-->
-  <!--            v-slot="{ errors }"-->
-  <!--            name="password"-->
-  <!--            rules="required"-->
-  <!--          >-->
-  <!--            <v-text-field-->
-  <!--              v-model="form.password"-->
-  <!--              type="password"-->
-  <!--              :error-messages="errors"-->
-  <!--              :label="$t('fields.password')"-->
-  <!--              required-->
-  <!--            ></v-text-field>-->
-  <!--          </validation-provider>-->
-
-  <!--          <v-btn-->
-  <!--            class="mr-4"-->
-  <!--            type="submit"-->
-  <!--            :loading="loading" :disabled="invalid || !validated || loading"-->
-  <!--          >-->
-  <!--            {{ $t('login') }}-->
-  <!--          </v-btn>-->
-  <!--          <v-btn @click="clear">-->
-  <!--            {{ $t('clear') }}-->
-  <!--          </v-btn>-->
-  <!--        </form>-->
-  <!--      </validation-observer>-->
-  <!--    </v-container>-->
-  <!--  </section>-->
 </template>
 <script>
 import {ValidationObserver, ValidationProvider} from 'vee-validate'
@@ -139,7 +92,6 @@ export default {
   },
   methods: {
     onSubmit() {
-      // console.log('submit')
       this.loading = true
       const isValid = this.$refs.observer.validate();
       if (isValid) {
