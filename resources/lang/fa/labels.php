0e1c011e67e6090df2584724deda2f17ec4a323c
Saeed Rostami <privatemx.2010@gmail.com>
2020-12-13 21:52:42 +0330

    Create labels.php

---
+++ resources/lang/fa/labels.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in labels throughout the system.
+    | Regardless where it is placed, a label can be listed here so it is easily
+    | found in a intuitive way.
+    |
+    */
+
+    'emails' => [
+        'contact' => [
+            'title'   => 'ارسال فرم تماس جدید!',
+            'name'    => 'نام',
+            'email'   => 'ایمیل',
+            'message' => 'پیغام',
+        ],
+    ],
+];
