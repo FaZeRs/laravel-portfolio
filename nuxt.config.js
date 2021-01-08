0148684ac2772ae96212b6d8b2f2e7d53dad257d
Nauris <naurislinde@gmail.com>
2021-01-08 22:25:06 +0200

    Updated Docker build

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ export default {
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: 'http://localhost/api'
+    baseURL: 'http://portfolio.test/api'
   },
   auth: {
     redirect: {
